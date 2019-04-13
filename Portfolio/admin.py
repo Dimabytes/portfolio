bad3e6a7cbff82065931dda6da66df52f15dfbbe
Dima <dimabytes@gmail.com>
2019-04-13 14:55:55 +0300

    add cool ja and css to model translate

--- Portfolio/admin.py
+++ Portfolio/admin.py
@@ -4,10 +4,28 @@ from modeltranslation.admin import TranslationAdmin
 
 
 class WorkAdmin(TranslationAdmin):
+    class Media:
+        js = (
+            'modeltranslation/js/force_jquery.js',
+            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.24/jquery-ui.min.js',
+            'modeltranslation/js/tabbed_translation_fields.js',
+        )
+        css = {
+            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
+        }
     pass
 
 
 class TechnologyAdmin(TranslationAdmin):
+    class Media:
+        js = (
+            'modeltranslation/js/force_jquery.js',
+            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.24/jquery-ui.min.js',
+            'modeltranslation/js/tabbed_translation_fields.js',
+        )
+        css = {
+            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
+        }
     pass
 
 
